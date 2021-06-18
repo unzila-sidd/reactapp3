294de71439d7b779722c6ad984ab051185b1adb5
unzila <unzila31@gmail.com>
2021-06-18 15:14:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{ useState } from 'react';
-import {Message} from './Message.js';
-export default function App() {
-  let [count, setCount] = useState(2);
-  let [isMorning, setMorning] = useState(false);
+import React, { useState } from 'react';
+import { Message } from './Message';
+import './App.css';
 
-  // setCount(2);
+export default function App(){
+  let [count , setCount] = useState(5);
+  let [isMorning , setMorning] = useState(true)
+
   return (
-    <div className={`box ${isMorning ? 'dayLight':''}`}>
-       <h1>Day Time = {isMorning ? 'Morning' : 'Night'}</h1>
-    
-      <Message counter={count}/>
-     <button onClick= {() =>setCount(++count)}>Update counter</button>
-     <button onClick= {() =>setMorning(!isMorning)}>Update Day</button>
+    <div class={`box ${isMorning ? 'dayLight' : ''}`}>
+
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+      <Message counter={count} />
+
+      <button onClick={() => setCount(count + 5)}>update count</button>
+      <button onClick={() => setMorning(!isMorning)}>Update day</button>
+
     </div>
+
   );
 }
-
